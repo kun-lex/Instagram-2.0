378d971261db9f14ffffb42e45d7001123074d1a
Dev.David <97886296+kun-lex@users.noreply.github.com>
2023-08-05 23:01:24 +0100

    update posts

--- components/post.js
+++ components/post.js
@@ -1,20 +1,12 @@
-import {
-    BookmarkIcon,
-    ChatIcon,
-    DotHorizontalIcon,
-    EmojiHappyIcon,
-    HeartIcon,
-    PaperAirplaneIcon,
-} from "@heroicons/react/outline";
+import { BookmarkIcon, ChatIcon, DotHorizontalIcon, EmojiHappyIcon, HeartIcon, PaperAirplaneIcon } from "@heroicons/react/outline";
 import {HeartIcon as HeartIconFilled} from "@heroicons/react/solid";
 
-import React from 'react'
 
 function Post({ id, username, userImg, img, caption }) {
   return (
     <div className="bg-white my-7 border rounded-sm" >
         <div className=" flex items-center p-5 ">
-            <img src='' alt='' className="rounded-full h-12 w-12 object-contain border p-1 mr-3"/>
+            <img src={userImg} alt='' className="rounded-full h-12 w-12 object-contain border p-1 mr-3"/>
             <p className="flex-1 font-bold" >{username}</p>
             <DotHorizontalIcon className='h-5' />
         </div>
@@ -32,4 +24,4 @@ function Post({ id, username, userImg, img, caption }) {
   )
 }
 
-export default Post
+export default Post;
--- components/posts.js
+++ components/posts.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Post from './post'
+import Post from "./post"
+
+const Posts_Data = [
+  {
+    id: '123',
+    username: 'dev_david',
+    userImg: 'https://links.papareact.com/3ke',
+    img: 'https://links.papareact.com/3ke',
+    caption: 'Testing'
+  },
+  {
+    id: '123',
+    username: 'dev_david',
+    userImg: 'https://links.papareact.com/3ke',
+    img: 'https://links.papareact.com/3ke',
+    caption: 'Testing'
+  }
+]
 
 function Posts() {
   return (
     <div>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+      {Posts_Data.map((post) => (
+        <Post key={post.id} id={post.id} username={post.username} userImg={post.userImg} img={post.img} caption={post.caption} />
+      ))}
     </div>
   )
 }
